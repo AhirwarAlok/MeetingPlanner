d79655fda0bf5d9633074b6e58eaaba2fd35b657
Ahirwar Alok <ahirwaralok@gmail.com>
2021-03-20 16:19:43 +0530

    Adds a page for list all rooms in UI

--- MeetingPlannerUI/urls.py
+++ MeetingPlannerUI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, rooms
 
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('rooms', rooms, name="rooms"),
 ]
--- MeetingPlannerUI/views.py
+++ MeetingPlannerUI/views.py
@@ -5,5 +5,10 @@ from .models import Room, Meeting
 def home(request):
     roomCount = Room.objects.count()
     meetingCount = Meeting.objects.count()
-    return render(request, 'MeetingPlannerUI/home.html',
+    return render(request, "MeetingPlannerUI/home.html",
                   {"roomCount": roomCount, "meetingCount": meetingCount})
+
+
+def rooms(request):
+    rooms = Room.objects.all()
+    return render(request, "MeetingPlannerUI/rooms.html", {"rooms": rooms})
